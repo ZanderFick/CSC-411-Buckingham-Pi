e4a53b2ddee47d84b0b8df0e1f339c76b5ea2707
Zander <zanderfick1@hotmail.com>
2013-02-18 05:38:07 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,79 +1,49 @@
-import Tkinter
-from Tkinter import *
-import xlrd
-import numpy as np
+try:
+        import wx
+        import wx.grid as gridlib
+except ImportError:
+    raise ImportError,"wxpython module required!"
 
-power_array = np.zeros([7,1])
-
-
-class Main_window(Frame):
-    
-    def __init__(self, parent):
-        Frame.__init__(self, parent, background="white")
-            
+class Pi_interface(wx.Frame):
+    def __init__(self,parent,id,title):
+        wx.Frame.__init__(self,parent,id,title,size=(750,600))
         self.parent = parent
-            
-        self.initUI()
-            
-    def initUI(self):
-        
-        self.parent.title("Buckingham Pi")
-        self.pack(fill=BOTH, expand=1)
-        
-        #set up the choice list
-        Wb = xlrd.open_workbook("Base Units.xls")
-        parameter_sheet = Wb.sheet_by_index(0)
-        
-        choose_parameter = Listbox(self,bg="grey",selectmode="SINGLE")         
-        
-        for r in range(1,parameter_sheet.nrows):
-            name = parameter_sheet.cell(r,0).value
-            choose_parameter.insert(END,name)
-        
-        choose_parameter.pack()       
-        choose_parameter.place(x=10,y=10)
-        
-        def add_click():
-            val = map(int, choose_parameter.curselection())[0]
-            
-            [row, col] = power_array.shape
-            
-            for c in range(2,8):   
-                power = parameter_sheet.cell(val,c).value
-                
-                
-            
-            
-        add_parameter = Button(self, text ="add parameter",command = add_click)
-        add_parameter.place(x=10,y= 180)
-        
-        box_display = Text(self,state="disabled",height=10,width=30)
-              
-        box_display.place(x=150,y= 10)
-        
-          
-            
+        self.initialize()
         
         
-            
-def main():
-    
-    root = Tk()
-    
-    w = 750 
-    h = 600
-    width_pos = (root.winfo_screenwidth()- w)/2
-    height_pos = (root.winfo_screenheight()- h)/2
-    
-    root.geometry('%dx%d+%d+%d'%(w, h, width_pos, height_pos))
-    
-    app = Main_window(root)
-        
-    root.mainloop()
-    
-    
-    display = Frame(root)
-    
 
-if __name__ == '__main__':
-    main()
+    def initialize(self):
+                
+        panel = wx.Panel(self,-1,(10,20),style=wx.SUNKEN_BORDER)
+ 
+        ColLabels = ["Name","L","T","M",u"\u03F4","N","I","J","","Pi1","Pi2"]
+        
+        sizer = wx.BoxSizer(wx.VERTICAL)
+ 
+        Values = gridlib.Grid(panel)
+        Values.CreateGrid(11, 11)
+        
+        
+        for column in range(11):
+            
+            Values.SetColLabelValue(column, ColLabels[column])
+            Values.AutoSizeColumns()    
+        
+        sizer.Add(Values, 1, wx.EXPAND)
+        panel.SetSizerAndFit(sizer)  
+        self.SetAutoLayout(True)
+        self.SetSizer(sizer)
+        
+
+        Values.Bind(gridlib.EVT_GRID_CELL_CHANGE, self.OnCellChange,) #<---I cannot Get this to work 
+        
+
+         
+    def OnCellChange(self, event):
+        print self.GetCellValue(event.GetRow(), event.GetCol()) #<-- so that I can simply retrieve the typed cell value
+        
+            
+if __name__ == "__main__":
+    app = wx.App()
+    frame = Pi_interface(None,-1,'PI Finder GUI').Show()
+    app.MainLoop()
