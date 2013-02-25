07f2f9047ab65fc465c0c0bd405f5a892798f461
Carl Sandrock <carl.sandrock@rockconsulting.co.za>
2013-02-25 11:36:24 +0200

    Minor fixes to style and fix of column addition loop

--- GUI.py
+++ GUI.py
@@ -7,86 +7,92 @@ try:
         import PI_Finder as PI
 except ImportError:
     raise ImportError,"PI_Finder module required!"
-    
+
 import numpy as np
 
 class Pi_interface(wx.Frame):
     def __init__(self,parent,id,title):
         wx.Frame.__init__(self,parent,id,title,size=(800,700))
         self.parent = parent
-        self.initialize()                
+        self.updating_columns = False
+        self.initialize()
 
     def initialize(self):
-                
+
         panel = wx.Panel(self,-1,(10,20),style=wx.SUNKEN_BORDER)
- 
+
         ColLabels = ["Name","L","T","M",u"\u03F4","N","I","J"," ","Pi1","Pi2"]
-        
+
+        Ncols = len(ColLabels)
+        Nrows = 10
+
         sizer = wx.BoxSizer(wx.VERTICAL)
- 
+
         self.values = gridlib.Grid(panel)
-        self.values.CreateGrid(11, 11)
-                
+        self.values.CreateGrid(Nrows, Ncols)
+
         sizer.Add(self.values, 1, wx.EXPAND)
-        panel.SetSizerAndFit(sizer)  
+        panel.SetSizerAndFit(sizer)
         self.SetAutoLayout(True)
-        self.SetSizer(sizer)        
+        self.SetSizer(sizer)
 
-        self.values.Bind(gridlib.EVT_GRID_CELL_CHANGE, self.OnCellChange,) 
+        self.values.Bind(gridlib.EVT_GRID_CELL_CHANGE, self.OnCellChange,)
 
-        [col, row]= [self.values.GetNumberCols(), self.values.GetNumberRows()]
-        
-        
-        for column in range(col):
-            
-            self.values.SetColLabelValue(column, ColLabels[column])
-            self.values.AutoSizeColumns()  
-        
-        for r in range (0,row):
-            for c in range (8,col):            
-                self.values.SetReadOnly(r,c)    
- 
+        for column, name in enumerate(ColLabels):
+            self.values.SetColLabelValue(column, name)
+
+        self.values.AutoSizeColumns()
+
+        for r in range(Nrows):
+            for c in range(8, Ncols):
+                self.values.SetReadOnly(r,c)
+
         self.input_mat = np.array([[0,0,0,0,0,0,0]])
-         
+
     def OnCellChange(self, event):
-        row = self.values.GetNumberRows()        
-       
+        if self.updating_columns:
+            return
+
+        Nrows = self.values.GetNumberRows()
+
         [m_row, m_col] = self.input_mat.shape
-        delt = row - m_row
-                
+        delt = Nrows - m_row
+
         if delt > 0:
             add_row = np.array([[0,0,0,0,0,0,0]])
             for r_add in range(delt):
-                self.input_mat = np.vstack((self.input_mat,add_row))    
+                self.input_mat = np.vstack((self.input_mat,add_row))
         if delt < 0:
             self.input_mat = self.input_mat[:+delt,:]
-    
+
         for update_col in range(1,7):
-            for update_row in range(0,row):
+            for update_row in range(0,Nrows):
                 val = self.values.GetCellValue(update_row, update_col)
                 if val == '':
                     self.input_mat[update_row,update_col] = 0
                 else:
-                    self.input_mat[update_row,update_col-1] = val 
-        
+                    self.input_mat[update_row,update_col-1] = val
+
         Result = PI.buck(self.input_mat)
         print Result
-       
+
         [res_rows, res_cols] = Result.shape
-        
+
         coldelt = res_cols-2
-        
-        ##if coldelt > 0:
-          ## self.values.InsertCols(11,coldelt) <-- this causes the program to loop
-        
+
+        if coldelt > 0:
+            self.updating_columns = True
+            self.values.InsertCols(11, coldelt)
+            self.updating_columns = False
+
         for resR in range(0,res_rows):
-            for colR in range(0,2): 
-                    val = "%g" % round(Result[resR,colR],2)  
+            for colR in range(0,2):
+                    val = "%g" % round(Result[resR,colR],2)
                     if colR <=2: #Due to the problem in line 80
                         self.values.SetCellValue(resR,colR+9,val)
-            
-            
-        
+
+
+
 if __name__ == "__main__":
     app = wx.App()
     frame = Pi_interface(None,-1,'PI Finder GUI').Show()
