2f14bb02056be25a451cfb8cd8789a796dcaf3de
Zander <zanderfick1@hotmail.com>
2013-03-04 00:42:38 +0200

    Program Fix
    Fixed row adding issues.
    Fixed result being read incorrectly into grid
    Added command to fill empty variable coefficient cells with zeros

--- GUI.py
+++ GUI.py
@@ -25,17 +25,17 @@ class Pi_interface(wx.Frame):
         ColLabels = ["Name", "L", "T", "M", u"\u03F4", "N", "I", "J"]
 
         Ncols = len(ColLabels)
-        Nrows = 10
+        Nrows_initial = 1
 
         sizer = wx.BoxSizer(wx.VERTICAL)
 
         self.values = gridlib.Grid(panel)
-        self.values.CreateGrid(Nrows, Ncols)
+        self.values.CreateGrid(Nrows_initial, Ncols)
 
         sizer.Add(self.values, 1, wx.EXPAND)
         panel.SetSizerAndFit(sizer)
-        self.SetAutoLayout(True)
-        self.SetSizer(sizer)
+        # self.SetAutoLayout(True)
+        # self.SetSizer(sizer)
 
         self.values.Bind(gridlib.EVT_GRID_CELL_CHANGE, self.OnCellChange,)
 
@@ -44,17 +44,20 @@ class Pi_interface(wx.Frame):
 
         self.values.AutoSizeColumns()
 
-        for r in range(Nrows):
+        for r in range(Nrows_initial):
             for c in range(8, Ncols):
                 self.values.SetReadOnly(r, c)
 
         self.input_mat = np.array([[0, 0, 0, 0, 0, 0, 0]])
 
     def OnCellChange(self, event):
+        Nempty_cells = 0
         if self.updating_columns:
+            Nempty_cells = 0
             return
 
         Nrows = self.values.GetNumberRows()
+        Ncols = self.values.GetNumberCols()
 
         [m_row, m_col] = self.input_mat.shape
         delt = Nrows - m_row
@@ -74,23 +77,45 @@ class Pi_interface(wx.Frame):
                 else:
                     self.input_mat[update_row, update_col-1] = val
 
-        Result = PI.buck(self.input_mat)
-        print Result
+        self.Result = PI.buck(self.input_mat)
+        print self.Result
 
-        [res_rows, res_cols] = Result.shape
+        for entry_row_check in range(0, Nrows):
+            Nempty_cells = 0
 
-        coldelt = res_cols-2
+            for addcheck in range(0, 8):
+                if self.values.GetCellValue(entry_row_check, addcheck) == '':
+                    if addcheck > 0:
+                        self.values.SetCellValue(entry_row_check, addcheck, '0')
+                        
+                    Nempty_cells += 1
+                if Nempty_cells == 8 and entry_row_check > 1:
+                    self.updating_columns = True
+                    self.values.DeleteRows(Nrows-1, 1)
+                    self.updating_columns = False
+
+                if Nempty_cells == 7 and entry_row_check == Nrows-1:
+                    self.updating_columns = True
+                    self.values.InsertRows(Nrows, 1)
+                    self.updating_columns = False
+
+        [res_rows, res_cols] = self.Result.shape
+        coldelt = res_cols-(Ncols-8)
 
         if coldelt > 0:
             self.updating_columns = True
-            self.values.InsertCols(11, coldelt)
+            self.values.InsertCols(Ncols, coldelt)
+            self.updating_columns = False
+
+        elif coldelt < 0:
+            self.updating_columns = True
+            self.values.DeleteCols(Ncols+coldelt, abs(coldelt))
             self.updating_columns = False
 
         for resR in range(0, res_rows):
-            for colR in range(0, 2):
-                    val = "%g" % round(Result[resR, colR], 2)
-                    if colR <= 2:   # Due to the problem in line 80
-                        self.values.SetCellValue(resR, colR+9, val)
+            for colR in range(0, res_cols):
+                val = "%g" % round(self.Result[resR, colR], 2)
+                self.values.SetCellValue(resR, colR+8, val)
 
 if __name__ == "__main__":
     app = wx.App()
