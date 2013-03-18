f33d3ce8b6c387a485e43af6d8607f070ff807e1
Zander <zanderfick1@hotmail.com>
2013-03-18 04:27:45 +0200

    General code cleanup

--- PI_Finder.py
+++ PI_Finder.py
@@ -4,57 +4,54 @@ import itertools
 from pandas import *
 from itertools import *
 from numpy import *
-from scipy import linalg, matrix
-from numpy import *
+
 
 def perm_set(a):
     if a == 0:
         return []
     elif a == 1:
-        return np.array([[0],[1]])
+        return np.array([[0], [1]])
     elif a > 1:
         p = a*a
-        d = np.zeros([1,p])
+        d = np.zeros([1, p])
 
-        for c1 in range(0, p+1): 
-    
+        for c1 in range(0, p+1):
+
             c2 = p-c1
             iter_mat = concatenate((np.ones([c1]), np.zeros([c2])), 0)
 
             for z in itertools.permutations(iter_mat, p):
-                intermediate = np.asmatrix(z)
                 d = np.concatenate((d, np.asmatrix(z)), 0)
         return DataFrame(d).drop_duplicates().values
 
 
 def buck(input_matrix):
-    [rows, cols] = input_matrix.shape   
-    
+    [rows, cols] = input_matrix.shape
+
     iszero = 1
-    for rowc in range(0,rows):   
+    for rowc in range(0, rows):
         row = rows - rowc - 1
-        for col in range(0,cols):
-            if input_matrix[row,col] <> 0:
+        for col in range(0, cols):
+            if input_matrix[row, col] != 0:
                 iszero = 0
         if iszero == 1:
-            input_matrix = np.delete(input_matrix,(row),axis=0) 
-            
-    [rows, cols] = input_matrix.shape       
-   
+            input_matrix = np.delete(input_matrix, (row), axis=0)
+
+    [rows, cols] = input_matrix.shape
+
     D_mat = input_matrix.T
 #Delete null rows
     D_mat = D_mat[D_mat.any(1)]
 
 #get matrix dimensions
     [d, k] = D_mat.shape
-   
 
     p = k-d
     if p < 0:
-        print ""  
-    #Is the handling of systems with more dimensions than variables feasable 
+        print ""
+    #Is the handling of systems with more dimensions than variables feasable
     #when taking into account the function of this project?
-    elif d >= 1 and k >= 1 :
+    elif d >= 1 and k >= 1:
 #Create righthand A-matrix and remove singularities
         A_det = 0
 
@@ -63,20 +60,19 @@ def buck(input_matrix):
             A_det = np.linalg.det(A_mat)
             A_rank = np.linalg.matrix_rank(A_mat)
             delta = d - A_rank
-       
-    
+
             if delta == 0 and A_det == 0:
 #randomly swap two columns
-                r = random.randint(0,k-1)
-                D_mat[:,[0,1+r]] =  D_mat[:,[1+r,0]]
-        
-            elif A_det == 0 and delta <> 0:
+                r = random.randint(0, k-1)
+                D_mat[:, [0, 1+r]] = D_mat[:, [1+r, 0]]
+
+            elif A_det == 0 and delta != 0:
 #remove singular rows and create new dimensional matrix
-                u,s,v = np.linalg.svd(A_mat)
-                D_mat = D_mat[s>1e-10]
+                u, s, v = np.linalg.svd(A_mat)
+                D_mat = D_mat[s > 1e-10]
                 [d, k] = D_mat.shape
                 p = k-d
-            #    
+
 #Create lefthand B-matrix
         B_mat = D_mat.T[0:p].T
 
@@ -85,56 +81,53 @@ def buck(input_matrix):
 
 #Create the E null patrix
 
-        null_mat = np.zeros([p,d])
+        null_mat = np.zeros([p, d])
 
 #Create the -inv(A)B submatrix
 
         A_inv = np.linalg.inv(A_mat)
 
-        invAB_mat = dot(A_inv,B_mat)*-1
+        invAB_mat = dot(A_inv, B_mat)*-1
 #Assemble the E matrix
 
-        E_mat_top = np.concatenate((I_mat,null_mat),axis = 1)
-        E_mat_bottom = np.concatenate((invAB_mat,A_inv),axis = 1)
+        E_mat_top = np.concatenate((I_mat, null_mat), axis=1)
+        E_mat_bottom = np.concatenate((invAB_mat, A_inv), axis=1)
 
-        E_mat = np.concatenate((E_mat_top,E_mat_bottom),axis=0)
+        E_mat = np.concatenate((E_mat_top, E_mat_bottom), axis=0)
 
 #Set up the Z matrix
 #Added advantage that other groups (not non-dimensional) can be found
-#simply by seting the last k rows to the desired type of dimensional group 
+#simply by seting the last k rows to the desired type of dimensional group
 #power values For the non dimensional case they are simply set to zero
 
 #create the q sub matrix for the non dimensional case
 
-        q_mat = np.zeros([d,p])
+        q_mat = np.zeros([d, p])
 
 #create the remaining non singular top Z sub matrix
-        
-        top_mat_perm =  perm_set(p)  
-        
-        [p_row, p_col] = top_mat_perm.shape     
+        top_mat_perm = perm_set(p)
+
+        [p_row, p_col] = top_mat_perm.shape
 
 # create the result matrix
-        Pi_result = np.zeros([rows,0])        
-        
+        Pi_result = np.zeros([rows, 0])
+
         for rescount in range(p_row):
-            top_vect = np.asmatrix(top_mat_perm[rescount])
+            t_vect = np.asmatrix(top_mat_perm[rescount])
             if p_row > 2:
-                top_mat = np.concatenate((top_vect[0,:p],top_vect[0,-p:]),0)
+                top_mat = np.concatenate((t_vect[0, :p], t_vect[0, -p:]), 0)
 
-            else: top_mat = top_vect           
-        
+            else:
+                top_mat = t_vect
 
-            Z_mat = np.concatenate((top_mat,q_mat),axis=0)
-
+            Z_mat = np.concatenate((top_mat, q_mat), axis=0)
 
 #the resulting P matrix columns represent the pi groups
 
-            Pi_mat = around((dot(E_mat,Z_mat)),decimals = 2)
-    
-            Pi_result = np.concatenate((Pi_result,Pi_mat),1)
-            
-            Pi_result = Pi_result.T[Pi_result.T.any(1)].T
+            Pi_mat = around((dot(E_mat, Z_mat)), decimals=2)
 
+            Pi_result = np.concatenate((Pi_result, Pi_mat), 1)
+
+            Pi_result = Pi_result.T[Pi_result.T.any(1)].T
 
         return Pi_result
--- test_pi_finder.py
+++ test_pi_finder.py
@@ -4,6 +4,7 @@ import unittest
 import numpy
 import PI_Finder
 
+
 class TestKnownValues(unittest.TestCase):
     def test_ones(self):
         a = numpy.array([[1, 1],
@@ -12,7 +13,6 @@ class TestKnownValues(unittest.TestCase):
         expected = numpy.array([[1],
                                 [-1]])
         numpy.testing.assert_almost_equal(calculated, expected)
-        
 
     def test_strange(self):
         a = numpy.array([[1, 0, 0],
@@ -22,6 +22,17 @@ class TestKnownValues(unittest.TestCase):
         expected = numpy.array([[1],
                                 [-1],
                                 [1]])
+        numpy.testing.assert_almost_equal(calculated, expected)
+
+    def test_pendulum(self):
+        a = numpy.array([[1, 0, 0, -2],
+                         [0, 1, 0, 0],
+                         [0, 0, 1, 1]]).T
+        calculated = PI_Finder.buck(a)
+        expected = numpy.array([[1],
+                                [0],
+                                [-0.5],
+                                [0.5]])
         numpy.testing.assert_almost_equal(calculated, expected)
 
 
