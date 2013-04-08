39acdc3b177e5bc85821655800511669b4c5559c
Carl Sandrock <carl.sandrock@rockconsulting.co.za>
2013-04-08 11:37:07 +0200

    Use the inverse of the a matrix

--- svd_PI_finder.py
+++ svd_PI_finder.py
@@ -4,7 +4,7 @@ from scipy.linalg import svd
 
 def svd_buck(input_matrix):
 
-    U, S, V = svd(input_matrix)
+    U, S, V = svd(input_matrix.T)
     zero_close = 1e-15
     S_mask = (S <= zero_close)
     null_space = sp.compress(S_mask, V, axis=0)
