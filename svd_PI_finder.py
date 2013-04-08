20fc944be11cc87768a8a81da27d892d3fbdd59b
Zander <zanderfick1@hotmail.com>
2013-04-08 03:34:11 +0200

    added svd program and tester
    The nullspace method just does not seem to work, I followed what methods
    I could find (neglecting rounding off etc for now)

---
+++ svd_PI_finder.py
@@ -0,0 +1,12 @@
+import scipy as sp
+from scipy.linalg import svd
+
+
+def svd_buck(input_matrix):
+
+    U, S, V = svd(input_matrix)
+    zero_close = 1e-15
+    S_mask = (S <= zero_close)
+    null_space = sp.compress(S_mask, V, axis=0)
+
+    return (sp.transpose(null_space))
