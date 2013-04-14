14161ae9a11be58355e63f94265995230f148b1b
Carl Sandrock <carl.sandrock@up.ac.za>
2013-04-14 20:15:16 +0200

    Improved SVD method PI finder

--- svd_PI_finder.py
+++ svd_PI_finder.py
@@ -1,12 +1,13 @@
 import scipy as sp
+import numpy
 from scipy.linalg import svd
 
-
-def svd_buck(input_matrix):
-
-    U, S, V = svd(input_matrix.T)
-    zero_close = 1e-15
-    S_mask = (S <= zero_close)
-    null_space = sp.compress(S_mask, V, axis=0)
-
-    return (sp.transpose(null_space))
+def svd_buck(input_matrix, eps=1e-15):
+    inshape = input_matrix.shape
+    padded_input_matrix = numpy.zeros([max(inshape)]*2)
+    padded_input_matrix[:inshape[0], :inshape[1]] = input_matrix
+    U, S, Vh = svd(padded_input_matrix.T)
+    null_space = Vh[S <= eps, :]
+    # make integer
+    null_space = null_space/numpy.ma.masked_equal(numpy.abs(null_space), 0).min(0)
+    return null_space.T
