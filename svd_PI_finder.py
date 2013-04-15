b7d4f9deef5b21e3b69612a7762a8261b3fc0094
Zander <zanderfick1@hotmail.com>
2013-04-15 05:36:16 +0200

    Rollback
    Rollback to the original SVD program as provided by carl with the
    working integer convertor

--- svd_PI_finder.py
+++ svd_PI_finder.py
@@ -3,22 +3,12 @@ import fractions
 from scipy.linalg import svd
 
 def buck(input_matrix, eps=1e-16):
-    
     inshape = input_matrix.shape
-    [c,r] = inshape
-    if c < r:
-        padded_input_matrix = numpy.zeros([max(inshape)]*2)
-        padded_input_matrix[:inshape[0], :inshape[1]] = input_matrix
-        U, S, Vh = svd(padded_input_matrix.T)
-        null_space = Vh[S <= eps, :]
-        print "padded input_mat" , padded_input_matrix.T
-    else:
-        U, S, Vh = svd(input_matrix.T)
-        null_space = Vh[S <= eps, :]
-        print "non padded input_mat" , input_matrix.T
-        
- 
+    padded_input_matrix = numpy.zeros([max(inshape)]*2)
+    padded_input_matrix[:inshape[0], :inshape[1]] = input_matrix
+    U, S, Vh = svd(padded_input_matrix.T)
+    null_space = Vh[S <= eps, :]
+
 # make integer
-    
-    print "null_space", null_space.T  
+    null_space = numpy.round(null_space/numpy.ma.masked_less(numpy.abs(null_space.T), 1e-10).min(0).T) 
     return null_space.T
